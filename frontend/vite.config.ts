e4bd753e0c70062517f6b1f48eeb142801105c82
Igor Bocharov <igor.bocharov56@yandex.ru>
2025-11-15 22:57:34 +0500

    fix(frontend): исправить импорт scss-переменных через настройку vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,27 +6,28 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [svgr(), react(), tsconfigPaths({ root: __dirname })],
-    resolve: {
-        alias: {
-            $fonts: resolve('./src/vendor/fonts'),
-            $assets: resolve('./src/assets'),
-        },
+  plugins: [svgr(), react(), tsconfigPaths({ root: __dirname })],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+      $fonts: resolve('./src/vendor/fonts'),
+      $assets: resolve('./src/assets'),
     },
-    build: {
-        assetsInlineLimit: 0,
-    },
-    css: {
-        preprocessorOptions: {
-            scss: {
-                additionalData: `
-          @use "./src/scss/variables" as *;
-          @use "./src/scss/mixins";
+  },
+  build: {
+    assetsInlineLimit: 0,
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+          @use "@/scss/variables" as *;
+          @use "@/scss/mixins";
         `,
-            },
-        },
+      },
     },
-    server: {
-        open: true,
-    },
+  },
+  server: {
+    open: true,
+  },
 })
