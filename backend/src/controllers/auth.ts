5c00452bf8b5d1965d0eb0072ba74e25d059ac96
Igor Bocharov <igor.bocharov56@yandex.ru>
2025-11-17 02:42:44 +0500

    refactor: вынести функцию для проверки типа ошибки в утилиты

--- backend/src/controllers/auth.ts
+++ backend/src/controllers/auth.ts
@@ -10,24 +10,7 @@ import ConflictError from '../errors/conflict-error'
 import NotFoundError from '../errors/not-found-error'
 import UnauthorizedError from '../errors/unauthorized-error'
 import User from '../models/user'
-
-// Интерфейс для ошибки Mongoose о дубликате, чтобы безопасно проверять error.code
-interface MongooseDuplicateKeyError extends Error {
-  code: number
-}
-
-// Функция-предикат (type guard) для проверки типа ошибки.
-// Это позволяет Typescript "узнать" тип ошибки внутри условного блока.
-function isMongooseDuplicateKeyError(
-  error: unknown
-): error is MongooseDuplicateKeyError {
-  return (
-    typeof error === 'object' &&
-    error !== null &&
-    'code' in error &&
-    (error as MongooseDuplicateKeyError).code === 11000
-  )
-}
+import { isMongooseDuplicateKeyError } from '../utils/error-helpers'
 
 // POST /auth/login
 const login = async (req: Request, res: Response, next: NextFunction) => {
