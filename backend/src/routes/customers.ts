2c80d9cd71b64ac5697cfb1fd1e57be2b6e13b74
Igor Bocharov <igor.bocharov56@yandex.ru>
2025-11-19 23:16:47 +0500

    feat(routes): ограничить доступ к управлению клиентами только для администраторов

--- backend/src/routes/customers.ts
+++ backend/src/routes/customers.ts
@@ -5,9 +5,16 @@ import {
   getCustomers,
   updateCustomer,
 } from '../controllers/customers'
-import auth from '../middlewares/auth'
+import auth, { roleGuardMiddleware } from '../middlewares/auth'
+import { Role } from '../models/user'
 
 const customerRouter = Router()
+
+// Все маршруты требуют аутентификации
+customerRouter.use(auth)
+
+// Добавляем guard. Только Admin может видеть список всех и удалять/править.
+customerRouter.use(roleGuardMiddleware(Role.Admin))
 
 customerRouter.get('/', auth, getCustomers)
 customerRouter.get('/:id', auth, getCustomerById)
